c63df74a591668377119bf669640fadac0640d2d
Shubham Choudhary <shubhamc183@gmail.com>
2021-02-22 21:53:44 +0530

    Run-Length Encoding shorter and easy solution

--- Run-Length Encoding/solution.py
+++ Run-Length Encoding/solution.py
@@ -17,3 +17,15 @@ def runLengthEncoding(string):
 	encoded_string += "%s%s" % (running_count, last_string)
 	return encoded_string
 
+## Algo Expert version which is short and easy
+def runLengthEncoding(string):
+    encoded_string = ""
+	running_count = 1
+	string_length = len(string)
+	for i in range(1, string_length):
+		if string[i-1] != string[i] or running_count == 9:
+			encoded_string += "%s%s" % (running_count, string[i-1])
+			running_count = 0
+		running_count += 1
+	encoded_string += "%s%s" % (running_count, string[string_length - 1])
+	return encoded_string
