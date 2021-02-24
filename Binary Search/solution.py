7a1a0d3c74d635db20f777b10578880d3531a0b2
Shubham Choudhary <shubhamc183@gmail.com>
2021-02-25 01:00:36 +0530

    Add Binary Search Solution

---
+++ Binary Search/solution.py
@@ -0,0 +1,15 @@
+# https://www.algoexpert.io/questions/Binary%20Search
+
+def binarySearch(array, target):
+	left_index = 0
+	right_index = len(array) - 1
+	while left_index <= right_index:
+		middle_index = int((left_index +right_index)/2)
+		if array[middle_index] == target:
+			return middle_index
+		elif array[middle_index] > target:
+			right_index = middle_index - 1
+		else:
+			left_index = middle_index + 1
+	return -1
+
