4e2be15bf6e31ae54d6d34c16b614dc122a7f8a2
Shubham Choudhary <shubhamc183@gmail.com>
2021-02-25 00:39:57 +0530

    Add Palindrome Check Solution

---
+++ Palindrome Check/solution.py
@@ -0,0 +1,21 @@
+# https://www.algoexpert.io/questions/Palindrome%20Check
+
+def isPalindrome(string):
+    string_length = len(string)
+	for i in range(int(string_length/2) + 1):
+		if string[i] != string[string_length - i - 1]:
+			return False
+    return True
+
+
+# Binary Search Style
+def isPalindrome(string):
+	i = 0
+	j = len(string) - 1
+	while i < j:
+		if string[i] != string[j]:
+			return False
+		i += 1
+		j -= 1
+	return True
+
