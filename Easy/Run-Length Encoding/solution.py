109e469f41ca11142b80b8b8f3f1dc20a7a5323d
Shubham Choudhary <shubhamc183@gmail.com>
2021-02-28 19:23:50 +0530

    Run-Length Encoding Add Complexity

--- Easy/Run-Length Encoding/solution.py
+++ Easy/Run-Length Encoding/solution.py
@@ -1,6 +1,6 @@
 # https://www.algoexpert.io/questions/Run-Length%20Encoding
 def runLengthEncoding(string):
-    encoded_string = ""
+	encoded_string = ""
 	last_string = string[0]
 	running_count = 1
 	for i in range(1, len(string)):
@@ -17,9 +17,12 @@ def runLengthEncoding(string):
 	encoded_string += "%s%s" % (running_count, last_string)
 	return encoded_string
 
+
 ## Algo Expert version which is short and easy
+# Time: O(n)
+# Space: O(n)
 def runLengthEncoding(string):
-    encoded_string = ""
+	encoded_string = ""
 	running_count = 1
 	string_length = len(string)
 	for i in range(1, string_length):
