2277f200c37026c47a51d24e7407406204266f2e
Shubham Choudhary <shubhamc183@gmail.com>
2021-02-28 19:18:19 +0530

    Binary Search Use Modulus Operator

--- Easy/Binary Search/solution.py
+++ Easy/Binary Search/solution.py
@@ -1,10 +1,13 @@
 # https://www.algoexpert.io/questions/Binary%20Search
 
+
+# Time: O(nlog(n))
+# Space: O(1)
 def binarySearch(array, target):
 	left_index = 0
 	right_index = len(array) - 1
 	while left_index <= right_index:
-		middle_index = int((left_index +right_index)/2)
+		middle_index = (left_index +right_index) // 2
 		if array[middle_index] == target:
 			return middle_index
 		elif array[middle_index] > target:
@@ -12,4 +15,3 @@ def binarySearch(array, target):
 		else:
 			left_index = middle_index + 1
 	return -1
-
