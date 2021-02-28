a7e235661ef2f6593d94193823697ef0761f74dd
Shubham Choudhary <shubhamc183@gmail.com>
2021-02-28 19:20:46 +0530

    Palindrome Check Minor

--- Easy/Palindrome Check/solution.py
+++ Easy/Palindrome Check/solution.py
@@ -1,13 +1,16 @@
 # https://www.algoexpert.io/questions/Palindrome%20Check
 
+# Time: O(n)
+# Space: O(1)
 def isPalindrome(string):
-    string_length = len(string)
+	string_length = len(string)
 	for i in range(int(string_length/2) + 1):
 		if string[i] != string[string_length - i - 1]:
 			return False
     return True
 
-
+# Time: O(n)
+# Space: O(1)
 # Binary Search Style
 def isPalindrome(string):
 	i = 0
