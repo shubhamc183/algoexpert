4f26ad0efb350c100bf1e060a098a31048f40e0b
Shubham Choudhary <shubhamc183@gmail.com>
2021-04-10 23:03:08 +0530

    Easy Tandem Bike refactor

--- Easy/Tandem Bike/solution.py
+++ Easy/Tandem Bike/solution.py
@@ -2,8 +2,8 @@
 # Space Complexity: O(1)
 def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
   redShirtSpeeds.sort(reverse=fastest)
-	blueShirtSpeeds.sort()
-	solution = 0
-	for i in range(len(redShirtSpeeds)):
-		solution += max(redShirtSpeeds[i], blueShirtSpeeds[i])
-    return solution
+  blueShirtSpeeds.sort()
+  solution = 0
+  for i in range(len(redShirtSpeeds)):
+    solution += max(redShirtSpeeds[i], blueShirtSpeeds[i])
+  return solution
