f419381b917fa6e124af792baf2c2eafb72f8a83
Shubham Choudhary <shubhamc183@gmail.com>
2021-04-10 20:53:48 +0530

    Easy - Tandem Bike

---
+++ Easy/Tandem Bike/solution.py
@@ -0,0 +1,9 @@
+# Time Complexity: O(NlogN) for quick sort
+# Space Complexity: O(1)
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+  redShirtSpeeds.sort(reverse=fastest)
+	blueShirtSpeeds.sort()
+	solution = 0
+	for i in range(len(redShirtSpeeds)):
+		solution += max(redShirtSpeeds[i], blueShirtSpeeds[i])
+    return solution
