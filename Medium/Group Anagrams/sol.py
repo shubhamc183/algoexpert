52a57ed98de2644bdb4c2db8c8173a23344e5b22
Shubham Choudhary <shubhamc183@gmail.com>
2021-04-13 16:54:02 +0530

    Medium - Group Anagrams Modified, reduced one loop

--- Medium/Group Anagrams/sol.py
+++ Medium/Group Anagrams/sol.py
@@ -2,11 +2,8 @@
 # S.C: O(w * n), n = length of longest word
 def groupAnagrams(words):
     anagrams = {}
-    solution = []
     for i in range(len(words)):
         word_sorted = ''.join(sorted(words[i]))
-        anagrams[word_sorted] = anagrams.get(word_sorted, []) + [i]
-    for anagram_indexes in anagrams.values():
-        solution.append([words[index] for index in anagram_indexes])
-    return solution
+        anagrams[word_sorted] = anagrams.get(word_sorted, []) + [words[i]]
+    return list(anagrams.values())
 
